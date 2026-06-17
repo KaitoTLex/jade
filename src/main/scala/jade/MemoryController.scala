f975b4b9ad1fab9b2c60501854a41900493f006c
Nluo923 <38440217+Nluo923@users.noreply.github.com>
2026-06-16 23:14:59 -0700

    fix formatting

--- src/main/scala/jade/MemoryController.scala
+++ src/main/scala/jade/MemoryController.scala
@@ -12,13 +12,13 @@ import chisel3.experimental.BundleLiterals._
 import chisel3.experimental.VecLiterals._
 
 class MemoryController(
-    addrBits:      Int,
-    dataBits:      Int,
-    numRequestors: Int = 4,
-    numChannels:   Int = 1, // Allocated based on external memory bandwidth
-    writeEnabled:  Boolean = true,
-    ageBits:       Int = 8,
-    ageThreshold:  Int = 128,
+    addrBits: Int,
+    dataBits: Int,
+    numRequestors: Int    = 4,
+    numChannels: Int      = 1, // Allocated based on external memory bandwidth
+    writeEnabled: Boolean = true,
+    ageBits: Int          = 8,
+    ageThreshold: Int     = 128
 ) extends Module {
   class ReadRequest extends Bundle {
     val address = UInt(addrBits.W)
@@ -26,8 +26,8 @@ class MemoryController(
   }
 
   class ReadResponse extends Bundle {
-    val data    = UInt(dataBits.W)
-    val from    = UInt(log2Ceil(numRequestors).W)
+    val data = UInt(dataBits.W)
+    val from = UInt(log2Ceil(numRequestors).W)
   }
 
   class WriteRequest extends Bundle {
@@ -37,10 +37,10 @@ class MemoryController(
   }
 
   class MemoryControllerIO(n: Int) extends Bundle {
-    val readReq   = Vec(n, Flipped(Decoupled(new ReadRequest)))  // requestor sends address
-    val readResp  = Vec(n, Decoupled(new ReadResponse))          // we forward data that dram returns
+    val readReq   = Vec(n, Flipped(Decoupled(new ReadRequest))) // requestor sends address
+    val readResp  = Vec(n, Decoupled(new ReadResponse)) // we forward data that dram returns
     val writeReq  = Vec(n, Flipped(Decoupled(new WriteRequest))) // requestor sends address and data
-    val writeResp = Vec(n, Decoupled(Bool()))                    // dram sends ack
+    val writeResp = Vec(n, Decoupled(Bool())) // dram sends ack
   }
 
   val io = IO(new Bundle {
@@ -49,53 +49,51 @@ class MemoryController(
   })
 
   // Age counted per requestor that increments per cycle
-  val readAge = RegInit(VecInit(Seq.fill(numRequestors)(0.U(ageBits.W))))
+  val readAge  = RegInit(VecInit(Seq.fill(numRequestors)(0.U(ageBits.W))))
   val writeAge = RegInit(VecInit(Seq.fill(numRequestors)(0.U(ageBits.W))))
 
   // Arbiter per channel that decides which requestor gets served
-  val readArb  = Seq.fill(numChannels)(Module(new RRArbiter(new ReadRequest, numRequestors)))
-  val writeArb = Seq.fill(numChannels)(Module(new RRArbiter(new WriteRequest, numRequestors)))
+  val readArb      = Seq.fill(numChannels)(Module(new RRArbiter(new ReadRequest, numRequestors)))
+  val writeArb     = Seq.fill(numChannels)(Module(new RRArbiter(new WriteRequest, numRequestors)))
   val readGranted  = WireDefault(VecInit(Seq.fill(numRequestors)(false.B)))
   val writeGranted = WireDefault(VecInit(Seq.fill(numRequestors)(false.B)))
 
   for (r <- 0 until numRequestors) {
-    io.req.readReq(r).ready := false.B
+    io.req.readReq(r).ready  := false.B
     io.req.writeReq(r).ready := false.B
   }
 
   for (c <- 0 until numChannels) {
-    val readStarving  = VecInit(readAge.map(_ > ageThreshold.U))
-    val writeStarving = VecInit(writeAge.map(_ > ageThreshold.U))
+    val readStarving     = VecInit(readAge.map(_ > ageThreshold.U))
+    val writeStarving    = VecInit(writeAge.map(_ > ageThreshold.U))
     val anyReadStarving  = readStarving.asUInt.orR
     val anyWriteStarving = writeStarving.asUInt.orR
-    val oldestRead  = PriorityEncoder(readStarving)
-    val oldestWrite = PriorityEncoder(writeStarving)
+    val oldestRead       = PriorityEncoder(readStarving)
+    val oldestWrite      = PriorityEncoder(writeStarving)
 
     // val resp = io.mem.readResp(c)
     // val ack = io.mem.writeResp(c)
 
     for (r <- 0 until numRequestors) {
       // if requestor is starving, ignore priority rules from arbiter
-      val forceRead  = anyReadStarving  && (oldestRead  === r.U)
+      val forceRead  = anyReadStarving && (oldestRead === r.U)
       val forceWrite = anyWriteStarving && (oldestWrite === r.U)
 
       // Arbiter ignores if request is already granted
-      readArb(c).io.in(r).valid := io.req.readReq(r).valid && !readGranted(r) || forceRead
-      readArb(c).io.in(r).bits  := io.req.readReq(r).bits
-      readArb(c).io.in(r).bits.from := r.U
-      writeArb(c).io.in(r).valid := io.req.writeReq(r).valid && !writeGranted(r) || forceWrite
-      writeArb(c).io.in(r).bits  := io.req.writeReq(r).bits
+      readArb(c).io.in(r).valid      := io.req.readReq(r).valid && !readGranted(r) || forceRead
+      readArb(c).io.in(r).bits       := io.req.readReq(r).bits
+      readArb(c).io.in(r).bits.from  := r.U
+      writeArb(c).io.in(r).valid     := io.req.writeReq(r).valid && !writeGranted(r) || forceWrite
+      writeArb(c).io.in(r).bits      := io.req.writeReq(r).bits
       writeArb(c).io.in(r).bits.from := r.U
 
       // increment age
-      when(io.req.readReq(r).valid)  { readAge(r)  := readAge(r) + 1.U }
-        .otherwise                   { readAge(r)  := 0.U }
-      when(io.req.writeReq(r).valid) { writeAge(r) := writeAge(r) + 1.U }
-        .otherwise                   { writeAge(r) := 0.U }
+      when(io.req.readReq(r).valid) { readAge(r) := readAge(r) + 1.U }.otherwise { readAge(r) := 0.U }
+      when(io.req.writeReq(r).valid) { writeAge(r) := writeAge(r) + 1.U }.otherwise { writeAge(r) := 0.U }
     }
 
     // arbiter out drives memory channels
-    io.mem.readReq(c) <> readArb(c).io.out
+    io.mem.readReq(c)  <> readArb(c).io.out
     io.mem.writeReq(c) <> writeArb(c).io.out
 
     // reset age on grant and update exlusion mask
@@ -114,12 +112,12 @@ class MemoryController(
     // channel accepts and transaction is completed
     when(readArb(c).io.out.fire) {
       val chosen = readArb(c).io.chosen
-      readAge(chosen) := 0.U
+      readAge(chosen)              := 0.U
       io.req.readReq(chosen).ready := true.B
     }
     when(writeArb(c).io.out.fire) {
       val chosen = writeArb(c).io.chosen
-      writeAge(chosen) := 0.U
+      writeAge(chosen)              := 0.U
       io.req.writeReq(chosen).ready := true.B
     }
   }
@@ -127,10 +125,10 @@ class MemoryController(
   // Responses need to be routed from channels back to original requestor
   // By default outputs shouldn't be valid
   for (r <- 0 until numRequestors) {
-    io.req.readResp(r).valid := false.B
-    io.req.readResp(r).bits := DontCare
+    io.req.readResp(r).valid  := false.B
+    io.req.readResp(r).bits   := DontCare
     io.req.writeResp(r).valid := false.B
-    io.req.writeResp(r).bits := DontCare
+    io.req.writeResp(r).bits  := DontCare
   }
 
   for (c <- 0 until numChannels) {
@@ -140,8 +138,8 @@ class MemoryController(
     // Reroute to requestors ID
     val readDest = readResp.bits.from
     io.req.readResp(readDest).valid := readResp.valid
-    io.req.readResp(readDest).bits := readResp.bits
-    readResp.ready := io.req.readResp(readDest).ready
+    io.req.readResp(readDest).bits  := readResp.bits
+    readResp.ready                  := io.req.readResp(readDest).ready
 
     val writeDest = writeAck.bits
     io.mem.writeResp(c).ready := true.B
--- src/main/scala/jade/Salu.scala
+++ src/main/scala/jade/Salu.scala
@@ -6,40 +6,40 @@ import chisel3.util._
 object SaluFn {
   private def fn(x: Int): UInt = x.U(11.W)
   // ISA funct
-  val SADD = fn(0x000) // sd = s1 + s2
+  val SADD  = fn(0x000) // sd = s1 + s2
   val SADDI = fn(0x001) // sd = s1 + sext(imm16)
-  val SSUB = fn(0x002) // sd = s1 - s2
-  val SMUL = fn(0x003) // sd = (s1 * s2)[31:0]
+  val SSUB  = fn(0x002) // sd = s1 - s2
+  val SMUL  = fn(0x003) // sd = (s1 * s2)[31:0]
   val SMULH = fn(0x004) // sd = (s1 * s2)[63:32], signed
-  val SDIV = fn(0x005) // signed divide
-  val SREM = fn(0x006) // signed remainder
-  val SAND = fn(0x010)
-  val SOR = fn(0x011)
-  val SXOR = fn(0x012)
-  val SNOT = fn(0x013) // sd = ~s1
-  val SSHL = fn(0x020) // logical shift left
-  val SSHR = fn(0x021) // logical shift right
-  val SSRA = fn(0x022) // arithmetic shift right
-  val SMIN = fn(0x030)
-  val SMAX = fn(0x031)
-  val SMOV = fn(0x040) // sd = s1
-  val SLI = fn(0x041) // sd = sext(imm16)
-  val SLUI = fn(0x042) // sd = imm16 << 16
+  val SDIV  = fn(0x005) // signed divide
+  val SREM  = fn(0x006) // signed remainder
+  val SAND  = fn(0x010)
+  val SOR   = fn(0x011)
+  val SXOR  = fn(0x012)
+  val SNOT  = fn(0x013) // sd = ~s1
+  val SSHL  = fn(0x020) // logical shift left
+  val SSHR  = fn(0x021) // logical shift right
+  val SSRA  = fn(0x022) // arithmetic shift right
+  val SMIN  = fn(0x030)
+  val SMAX  = fn(0x031)
+  val SMOV  = fn(0x040) // sd = s1
+  val SLI   = fn(0x041) // sd = sext(imm16)
+  val SLUI  = fn(0x042) // sd = imm16 << 16
 }
 
 //SALU inputs
 class SaluReq extends Bundle {
   val funct11 = UInt(11.W)
-  val s1 = UInt(32.W) // scalar GPR operand 1
-  val s2 = UInt(32.W) // scalar GPR operand 2 (r type only)
-  val imm16 = UInt(16.W) // immediate
+  val s1      = UInt(32.W) // scalar GPR operand 1
+  val s2      = UInt(32.W) // scalar GPR operand 2 (r type only)
+  val imm16   = UInt(16.W) // immediate
 }
 
 //Core
 class Salu extends Module {
   val io = IO(new Bundle {
     val req = Flipped(Valid(new SaluReq))
-    val sd = Valid(UInt(32.W))
+    val sd  = Valid(UInt(32.W))
   })
 
   val f = io.req.bits.funct11
@@ -57,8 +57,8 @@ class Salu extends Module {
   val prod64 = (aS * bS).asUInt // 64 bits wide
 
   val divByZero = b === 0.U
-  val quot = Mux(divByZero, ~0.U(32.W), ((aS / bS).asUInt)(31, 0))
-  val rem = Mux(divByZero, a, ((aS % bS).asUInt)(31, 0))
+  val quot      = Mux(divByZero, ~0.U(32.W), ((aS / bS).asUInt)(31, 0))
+  val rem       = Mux(divByZero, a, ((aS % bS).asUInt)(31, 0))
 
   val sd = WireDefault(0.U(32.W))
   switch(f) {
@@ -84,7 +84,7 @@ class Salu extends Module {
   }
 
   io.sd.valid := io.req.valid
-  io.sd.bits := sd
+  io.sd.bits  := sd
 
   when(io.req.valid && f === SaluFn.SADD) {
     assert(
@@ -103,14 +103,14 @@ class Salu extends Module {
 class SaluPipelined extends Module {
   val io = IO(new Bundle {
     val req = Flipped(Valid(new SaluReq))
-    val sd = Valid(UInt(32.W))
+    val sd  = Valid(UInt(32.W))
   })
 
   val core = Module(new Salu)
   core.io.req := io.req
 
   io.sd.valid := RegNext(core.io.sd.valid, init = false.B)
-  io.sd.bits := RegNext(core.io.sd.bits)
+  io.sd.bits  := RegNext(core.io.sd.bits)
 }
 
 /* sv synthesis */
