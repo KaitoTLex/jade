d30a52cec3d8afc95750f666ca5641e6e56c9e96
KaitoTLex <renl@kaitotlex.systems>
2026-06-29 21:36:56 -0700

    feat: simple alu stest

---
+++ docs/kaitotlex.md
@@ -0,0 +1,5 @@
+# KaitoTLex's development log:
+
+
+## Spontanous thoughts
+I think development wise, there exist potential for extensions to the ISA, for example a Ray tracing extension, path tracing extension etc. 
---
+++ src/test/jade/units/Sfu.scala
@@ -0,0 +1,7 @@
+package jade.units
+
+import chisel3._
+import chisel3.simulator.scalatest.ChiselSim
+import chisel3.simulator.stimulus.{RunUntilFinished, RunUntilSuccess}
+import chisel3.util.Counter
+import org.scalatest.funspec.AnyFunSpec
